eccd8b80a7b188cdede3c6ea2908985d3766eb39
nui <ppnuie@gmail.com>
2016-04-06 01:19:11 +0700

    remove unused

--- FANN/red or black/test.py
+++
@@ -1,6 +0,0 @@
-from fann2 import libfann
-
-ann = libfann.neural_net()
-ann.create_from_file("redOrBlack.net")
-
-print ann.run([0.535915943213, 2.80578521309 ,2.10856436755, 3.25266536956, 6.0806720787, 4.6799151241, -6.08696396883])
